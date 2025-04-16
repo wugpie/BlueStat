2dda0c5f7d1ba8b653559eaa5af63cde4a6b84c2
wugpie <wugpie@gmail.com>
2025-04-16 23:59:14 +0900

    1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("com.mojang:brigadier:1.0.18")
 }
