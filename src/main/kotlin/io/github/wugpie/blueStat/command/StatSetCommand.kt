03f75468e98eda93640e876875d811e08e77e656
wugpie <wugpie@gmail.com>
2025-04-29 00:05:03 +0900

    Finishing setStat Command

--- src/main/kotlin/io/github/wugpie/blueStat/command/StatSetCommand.kt
+++ src/main/kotlin/io/github/wugpie/blueStat/command/StatSetCommand.kt
@@ -5,6 +5,7 @@ import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.exceptions.CommandSyntaxException
+import io.github.wugpie.blueStat.util.setStat
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands.argument
 import org.bukkit.entity.Player
@@ -37,8 +38,32 @@ object StatSetCommand {
 
                                 val statName = stat
                                 val statValue = IntegerArgumentType.getInteger(context, "statValue")
-                                player.sendMessage(statName)
-                                player.sendMessage(statValue.toString())
+                                when(statName){
+                                    "strength" -> {
+                                        player.setStat("ATK", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §c공격력 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                    "concentration" -> {
+                                        player.setStat("RGW", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §6집중 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                    "defense" -> {
+                                        player.setStat("DEF", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §b방어력 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                    "aviation" -> {
+                                        player.setStat("AVI", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §a회피 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                    "stability" -> {
+                                        player.setStat("STB", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §e안정 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                    "critical" -> {
+                                        player.setStat("CRT", statValue)
+                                        player.sendMessage("§e${player.name}§f님의 §4치명타 §e스탯§f을 §e${statValue}§f로 §e변경했습니다")
+                                    }
+                                }
                                 1
                             }
                     )
